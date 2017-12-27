b4426d7caaf8e88d962dc10595e562065620f784
Mauricio Mercado <mau@maumercado.com>
2017-12-26 21:15:28 -0800

    Firebase setup

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import { AppRegistry, View, Text } from "react-native";
 import { Header } from "./src/components/common";
+import firebase from "firebase";
 
 export default class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyB-7ErC1c7WAmNxm6ZTufAtJpK8HP57yi8",
+            authDomain: "auth-react-native-accbe.firebaseapp.com",
+            databaseURL: "https://auth-react-native-accbe.firebaseio.com",
+            projectId: "auth-react-native-accbe",
+            storageBucket: "auth-react-native-accbe.appspot.com",
+            messagingSenderId: "876070193723"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <View>
