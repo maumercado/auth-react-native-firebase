aa5a3a339d8b7d36af8f399bceec7b5250d2acd5
Mauricio Mercado <mau@maumercado.com>
2017-12-27 16:50:57 -0800

    Fixed issue with logout button not showing properly

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { CardSection } from "./";
 import { Text, TouchableOpacity } from "react-native";
 
 const Button = ({ onPress, children }) => {
     const { buttonStyle, textStyle } = styles;
     return (
-        <TouchableOpacity onPress={onPress} style={buttonStyle}>
-            <Text style={textStyle}>{children}</Text>
-        </TouchableOpacity>
+        <CardSection>
+            <TouchableOpacity onPress={onPress} style={buttonStyle}>
+                <Text style={textStyle}>{children}</Text>
+            </TouchableOpacity>
+        </CardSection>
     );
 };
 
 const styles = {
     textStyle: {
         alignSelf: "center",
-        color: "#007AFF",
+        color: "#007aff",
         fontSize: 16,
         fontWeight: "600",
         paddingTop: 10,
@@ -22,10 +25,10 @@ const styles = {
     buttonStyle: {
         flex: 1,
         alignSelf: "stretch",
-        backgroundColor: "#FFF",
+        backgroundColor: "#fff",
         borderRadius: 5,
         borderWidth: 1,
-        borderColor: "#007AFF",
+        borderColor: "#007aff",
         marginLeft: 5,
         marginRight: 5
     }
